93637fee5d42d0dce809c4573fc3028d80a42795
IgorChvyrov-sm <igor.chvyrov@siliconmint.com>
2025-07-17 15:56:30 +0300

    fix: add swagger attributes to KafkaPublishInput

--- src/conductor/client/workflow/task/kafka_publish_input.py
+++ src/conductor/client/workflow/task/kafka_publish_input.py
@@ -5,6 +5,28 @@ from typing_extensions import Self
 
 
 class KafkaPublishInput:
+    swagger_types = {
+        '_bootstrap_servers': 'str',
+        '_key': 'str',
+        '_key_serializer': 'str',
+        '_value': 'str',
+        '_request_timeout_ms': 'str',
+        '_max_block_ms': 'str',
+        '_headers': 'dict[str, Any]',
+        '_topic': 'str',
+    }
+
+    attribute_map = {
+        '_bootstrap_servers': 'bootStrapServers',
+        '_key': 'key',
+        '_key_serializer': 'keySerializer',
+        '_value': 'value',
+        '_request_timeout_ms': 'requestTimeoutMs',
+        '_max_block_ms': 'maxBlockMs',
+        '_headers': 'headers',
+        '_topic': 'topic',
+    }
+
     def __init__(self,
                  bootstrap_servers: str = None,
                  key: str = None,
