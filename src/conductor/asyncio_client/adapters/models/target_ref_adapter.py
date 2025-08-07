15a3a24f87f65807e8958fe62164c7d0bc20b876
IgorChvyrov-sm <igor.chvyrov@siliconmint.com>
2025-08-07 16:21:04 +0300

    Added async authorization, integration, metadata, prompt clients tests and moved shared enums

--- src/conductor/asyncio_client/adapters/models/target_ref_adapter.py
+++ src/conductor/asyncio_client/adapters/models/target_ref_adapter.py
@@ -1,4 +1,10 @@
+from pydantic import field_validator
+
 from conductor.asyncio_client.http.models import TargetRef
 
 
-class TargetRefAdapter(TargetRef): ...
+class TargetRefAdapter(TargetRef):
+    @field_validator("id")
+    def id_validate_enum(cls, value):
+        """Validates the enum"""
+        return value
