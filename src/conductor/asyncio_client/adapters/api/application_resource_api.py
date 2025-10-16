22d11d9b554aaaf3e5e3bff516357a940f3f7145
IgorChvyrov-sm <igor.chvyrov@siliconmint.com>
2025-10-16 20:27:12 +0300

    Empty string for application create_access_key permissions not allowed

--- src/conductor/asyncio_client/adapters/api/application_resource_api.py
+++ src/conductor/asyncio_client/adapters/api/application_resource_api.py
@@ -1,4 +1,15 @@
+from pydantic import StrictStr
+
 from conductor.asyncio_client.http.api import ApplicationResourceApi
 
 
-class ApplicationResourceApiAdapter(ApplicationResourceApi): ...
+class ApplicationResourceApiAdapter(ApplicationResourceApi):
+    async def create_access_key(
+        self,
+        id: StrictStr,
+        *args,
+        **kwargs,
+    ) -> object:
+        if not id:
+            id = None
+        return await super().create_access_key(id=id, *args, **kwargs)
